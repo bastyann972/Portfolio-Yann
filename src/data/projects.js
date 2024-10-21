ab898c8bf781d8c0710b962918a6dc65569de628
bastin <bestyann972@gmail.com>
2024-10-21 08:10:55 -0400

    corection

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,21 +3,23 @@ const projects = [
     id: 1,
     title: "Okemon",
     description: "App qui permet de lister les cartes Pokémon avec leur prix",
-    image: "/Portfolio-Yann/img/pg1.webp", // Ne pas inclure "public" dans le chemin
+    image: "/Portfolio-Yann/img/pg1.webp",
     url: "https://github.com/bastyann972/okemon",
   },
   {
     id: 2,
     title: "Au chouette maraicher",
     description: "Site dynamique pour présenter les fruits et légumes",
-    image: "/Portfolio-Yann/img/pg3.webp", // Ne pas inclure "public"
+    image: "/Portfolio-Yann/img/pg3.webp",
     url: "https://github.com/bastyann972/Atelier-Omarraicher",
   },
   {
     id: 3,
     title: "Tripodvisor",
     description: "Site web d'une agence de voyage",
-    image: "/Portfolio-Yann/img/pg2.webp", // Ne pas inclure "public"
+    image: "/Portfolio-Yann/img/pg2.webp",
     url: "https://github.com/bastyann972/Atelier-Tripodvisor",
   },
 ];
+
+export default projects; // Ceci fait un export par défaut
